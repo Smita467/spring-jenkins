9d4fdf0f04693b9fa41981a86bddee9d20eb7318
Smita467 <48005495+Smita467@users.noreply.github.com>
2022-02-28 13:00:34 +0530

    second commit

--- src/test/java/com/example/jenkins/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/example/jenkins/api/SpringJenkinsApplicationTests.java
@@ -15,6 +15,7 @@ class SpringJenkinsApplicationTests {
 	@Test
 	void contextLoads() {
 		logger.info("Test case excuting.... ");
+		logger.info("Test case excuting second log statment.... ");
 		assertEquals(true, true);
 	}
 
